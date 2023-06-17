47bb67e64506a980d1c3bd26cf0fcba34d84b91c
benmelz <ben@melz.me>
2023-06-17 16:53:41 -0400

    build lambda validators

--- lib/limitable.rb
+++ lib/limitable.rb
@@ -20,26 +20,9 @@ module Limitable
 
         case column.type
         when :integer
-          max = (1 << ((limit * 8) - 1)) - 1
-          min = -max
-          klass.validate do
-            value = begin
-              self.class.type_for_attribute(column_name).serialize self[column_name]
-            rescue ActiveModel::RangeError => e
-              e.message.match(/(?<number>\d+) is out of range/)[:number].to_i
-            end
-            next unless value.is_a?(Integer)
-
-            errors.add column_name, I18n.t('errors.messages.greater_than_or_equal_to', count: min) if value < min
-            errors.add column_name, I18n.t('errors.messages.less_than_or_equal_to', count: max) if value > max
-          end
+          klass.validate(&build_integer_limit_validator(column_name, limit))
         when :string, :text
-          klass.validate do
-            value = self.class.type_for_attribute(column_name).serialize self[column_name]
-            next unless value.is_a?(String) && value.bytesize > limit
-
-            errors.add column_name, I18n.t('errors.messages.too_long.other', count: limit)
-          end
+          klass.validate(&build_string_limit_validator(column_name, limit))
         end
       end
     end
@@ -50,6 +33,31 @@ module Limitable
       klass.column_names
     rescue ActiveRecord::ActiveRecordError, ArgumentError
       []
+    end
+
+    def build_integer_limit_validator(column_name, limit)
+      max = (1 << ((limit * 8) - 1)) - 1
+      min = -max
+      lambda do
+        value = begin
+          self.class.type_for_attribute(column_name).serialize self[column_name]
+        rescue ActiveModel::RangeError => e
+          e.message.match(/(?<number>\d+) is out of range/)[:number].to_i
+        end
+        next unless value.is_a?(Integer)
+
+        errors.add column_name, I18n.t('errors.messages.greater_than_or_equal_to', count: min) if value < min
+        errors.add column_name, I18n.t('errors.messages.less_than_or_equal_to', count: max) if value > max
+      end
+    end
+
+    def build_string_limit_validator(column_name, limit)
+      lambda do
+        value = self.class.type_for_attribute(column_name).serialize self[column_name]
+        next unless value.is_a?(String) && value.bytesize > limit
+
+        errors.add column_name, I18n.t('errors.messages.too_long.other', count: limit)
+      end
     end
   end
 end
